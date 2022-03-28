f97bacfb403b2c73c3de057818ae1a89ff789701
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2022-03-28 12:49:44 -0400

    Working on integrating plist serialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate serde_derive;
+extern crate plist;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use anyhow::Error;
+use directories::{UserDirs};
+
+fn main() -> Result<(), Error> {
+    if let Some(user_dirs) = UserDirs::new() {
+        let audio_dir = match user_dirs.audio_dir() {
+            Some(dir) => { dir }
+            None => { panic!("Failed to locate audio directory"); }
+        };
+
+        let itunes_xml = audio_dir.join("iTunes\\iTunes Music Library.xml");
+
+        let fp = File::open(itunes_xml)?;
+        let fp = BufReader::new(fp);
+
+        let plist = plist::from_reader_xml(fp)?;
+
+        print!("{:?}", plist);
+    };
+
+    return Ok(())
 }
